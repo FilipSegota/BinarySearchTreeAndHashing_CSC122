6f6be3349885adcec7e171dbd045558ba3519d0e
Filip <70112731+FilipSegota@users.noreply.github.com>
2023-10-22 16:05:44 -0400

    Add Program files
    Add all the files.

---
+++ DataStructure.java
@@ -0,0 +1,19 @@
+/*****************************************************************************
+ * /* Author: Filip Segota
+ * /* Class: CSC 122, Fall 2020
+ * /* Assignment: Lab 6: Binary Search Tree and Hash Table
+ * /* File: Data Structure Interface
+ * /
+ *****************************************************************************/
+public interface DataStructure {
+    // methods
+    public void add(Student student);
+
+    public Student find(String name);
+
+    public int getCompares();
+
+    public void resetCompares();
+
+    public int getSize();
+}
